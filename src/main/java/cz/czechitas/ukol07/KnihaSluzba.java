3c76567e87e69cbbbe707ff5dc4b557c2a10e6ee
Anna <anickab99@gmail.com>
2025-06-22 17:18:23 +0200

    KnihaSluzba metody

--- src/main/java/cz/czechitas/ukol07/KnihaSluzba.java
+++ src/main/java/cz/czechitas/ukol07/KnihaSluzba.java
@@ -10,21 +10,55 @@ import java.util.List;
 
 public class KnihaSluzba {
 
-    private final List<Kniha> knihaList;
+    private final List<Kniha> seznamKnih;
 
     public KnihaSluzba() throws IOException {
         ObjectMapper objectMapper = new ObjectMapper();
 
         try (InputStream inputStream = KnihaSluzba.class.getResourceAsStream("knihy.json")) {
-            knihaList = objectMapper.readValue(inputStream, new TypeReference<List<Kniha>>() {});
+            seznamKnih = objectMapper.readValue(inputStream, new TypeReference<List<Kniha>>() {});
 
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
     }
 
-    public List<Kniha> getKnihaList() {
-        return knihaList;
+    public List<Kniha> getSeznamKnih() {
+        return seznamKnih;
+    }
+
+    public List<String> vypisVsechnyKnihy() {
+        List<String> vysledek = new ArrayList<>();
+        for (Kniha seznam : seznamKnih) {
+            vysledek.add(String.valueOf(seznam));
+        }
+        return vysledek;
+    }
+
+    public List<String> vypisKnihyOdAutora(String autor) {
+        List<String> vysledek = new ArrayList<>();
+        for (Kniha seznam : seznamKnih) {
+            if (seznam.getAutor() == autor) {
+                vysledek.add(String.valueOf(seznam));
+            }
+        } if (vysledek.isEmpty()) {
+            return vysledek;
+        } else {
+            return vysledek;
+        }
+    }
+
+    public List<String> vypisKnihyVydaneVRoce(String rokVydani) {
+        List<String> vysledek = new ArrayList<>();
+        for (Kniha seznam : seznamKnih) {
+            if (seznam.getRokVydani() == rokVydani) {
+                vysledek.add(String.valueOf(seznam));
+            }
+        } if (vysledek.isEmpty()) {
+            return vysledek;
+        } else {
+            return vysledek;
+        }
     }
 
 }
